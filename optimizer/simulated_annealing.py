909e5d79b1e73e33dc28f326f1f9aea46ff12f91
Renan Brito Butkeraites <rbritobut?@gmail.com>
2020-11-04 20:34:17 -0300

    Created objective function aquisition.

--- optimizer/simulated_annealing.py
+++ optimizer/simulated_annealing.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 class SimulatedAnnealing:
-    def __init__(self):
+    def __init__(self, objective):
         self.status = []
-        self.status.append("[OK] Simulated annealing created successfully")
+        self.__objective_aquisition(objective)
+
+    def __objective_aquisition(self, objective):
+        if objective:
+            if hasattr(objective, '__call__'):
+                self.status.append("[OK] Simulated annealing created successfully")
+                self.objective = objective
+            else:
+                self.status.append("[ERROR] Objective function must be a python function object")
+        else:
+            self.status.append("[ERROR] Invalid objective function")
--- test/test_simulated_annealing.py
+++ test/test_simulated_annealing.py
@@ -1,6 +1,18 @@
 import pytest
+import numpy as np
 from optimizer.simulated_annealing import SimulatedAnnealing
 
+def objective_function(np_variable):
+    return np.sum(np_variable)
+
 def test_simulated_annealing_init():
-    optimizer = SimulatedAnnealing()
+    optimizer = SimulatedAnnealing(objective_function)
     assert "[OK] Simulated annealing created successfully" in optimizer.status
+
+def test_simulated_annealing_wrong_format_objective_function():
+    optimizer = SimulatedAnnealing(1)
+    assert "[ERROR] Objective function must be a python function object" in optimizer.status
+
+def test_simulated_annealing_missing_objective_function():
+    optimizer = SimulatedAnnealing({})
+    assert "[ERROR] Invalid objective function" in optimizer.status
