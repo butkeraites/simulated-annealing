490d868992a7607d06c62273c86e9befc0a2fb91
Renan Brito Butkeraites <rbritobut?@gmail.com>
2020-11-05 00:09:00 -0300

    Cooling factor included to be positive and numeric

--- optimizer/simulated_annealing.py
+++ optimizer/simulated_annealing.py
@@ -1,27 +1,57 @@
 import numpy as np
+import pandas as pd
 
 class SimulatedAnnealing:
-    def __init__(self, objective, perturbation, cooling, stopping_criterion):
+    def __init__(self, objective, perturbation, cooling, stopping_criterion, initial_solution, cooling_factor):
         self.status = []
         self.controller = {}
+        self.command = "GO"
+        self.__initial_solution_aquisition(initial_solution)
+        self.__cooling_factor_aquisition(cooling_factor)
         self.__function_aquisition(objective, 'objective')
         self.__function_aquisition(perturbation, 'perturbation')
         self.__function_aquisition(cooling, 'cooling')
         self.__function_aquisition(stopping_criterion, 'stopping_criterion')
         self.__final_validation()
 
-    def __function_aquisition(self, function, function_name):
-        if function:
-            if hasattr(function, '__call__'):
-                self.status.append("[OK] Aquisition of {} done successfully".format(function_name))
-                self.controller[function_name] = function
+    def __initial_solution_aquisition(self, initial_solution):
+        if self.command == "GO":
+            if isinstance(initial_solution,(list,pd.core.series.Series,np.ndarray)):
+                self.status.append("[OK] Aquisition of initial solution done successfully")
+                self.initial_solution = np.ndarray(initial_solution)
             else:
-                self.status.append("[ERROR]<{}> A controller function must be a python function object".format(function_name))
-        else:
-            self.status.append("[ERROR] Invalid {} function".format(function_name))
+                self.status.append("[ERROR]<initial solution> The initial solution must be a list, pandas series or numpy ndarray")
+                self.command = "NO GO"
+
+    def __cooling_factor_aquisition(self, cooling_factor):
+        if self.command == "GO":
+            if isinstance(cooling_factor, (float, int)):
+                if cooling_factor < 1.0 and cooling_factor > 0.0:
+                    self.status.append("[OK] Aquisition of cooling factor done successfully")
+                    self.cooling_factor = np.float(cooling_factor)
+                else:
+                    self.status.append("[ERROR] The cooling factor must be positive and less than 1.0")
+                    self.command = "NO GO"
+            else:
+                self.status.append("[ERROR]<cooling factor> The cooling factor must be a float or int")
+                self.command = "NO GO"
+
+    def __function_aquisition(self, function, function_name):
+        if self.command == "GO":
+            if function:
+                if hasattr(function, '__call__'):
+                    self.status.append("[OK] Aquisition of {} done successfully".format(function_name))
+                    self.controller[function_name] = function
+                else:
+                    self.status.append("[ERROR]<{}> A controller function must be a python function object".format(function_name))
+                    self.command = "NO GO"
+            else:
+                self.status.append("[ERROR] Invalid {} function".format(function_name))
+                self.command = "NO GO"
 
     def __final_validation(self):
         if any("[ERROR]" in status for status in self.status):
             self.status.append("[ERROR] Simulated annealing creation failed")
+            self.command = "NO GO"
         else:
             self.status.append("[OK] Simulated annealing created successfully")
--- test/test_simulated_annealing.py
+++ test/test_simulated_annealing.py
@@ -16,38 +16,59 @@ def cooling_function(temperature, factor):
 def stopping_criterion(n_iteration, n_max):
     return n_iteration > n_max
 
+n_itens = 10
+initial_solution = np.random.binomial(1, .5, n_itens)
+
+cooling_factor = 1e-6
+
 def test_simulated_annealing_init():
-    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, stopping_criterion)
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, stopping_criterion, initial_solution, cooling_factor)
     assert "[OK] Simulated annealing created successfully" in optimizer.status
 
 def test_simulated_annealing_wrong_format_objective_function():
-    optimizer = SimulatedAnnealing(1,perturbation_function, cooling_function, stopping_criterion)
+    optimizer = SimulatedAnnealing(1, perturbation_function, cooling_function, stopping_criterion, initial_solution, cooling_factor)
     assert "[ERROR]<objective> A controller function must be a python function object" in optimizer.status
 
 def test_simulated_annealing_missing_objective_function():
-    optimizer = SimulatedAnnealing({},perturbation_function, cooling_function, stopping_criterion)
+    optimizer = SimulatedAnnealing({}, perturbation_function, cooling_function, stopping_criterion, initial_solution, cooling_factor)
     assert "[ERROR] Invalid objective function" in optimizer.status
 
 def test_simulated_annealing_wrong_format_perturbation_function():
-    optimizer = SimulatedAnnealing(objective_function,1, cooling_function, stopping_criterion)
+    optimizer = SimulatedAnnealing(objective_function, 1, cooling_function, stopping_criterion, initial_solution, cooling_factor)
     assert "[ERROR]<perturbation> A controller function must be a python function object" in optimizer.status
 
 def test_simulated_annealing_missing_perturbation_function():
-    optimizer = SimulatedAnnealing(objective_function,{}, cooling_function, stopping_criterion)
+    optimizer = SimulatedAnnealing(objective_function, {}, cooling_function, stopping_criterion, initial_solution, cooling_factor)
     assert "[ERROR] Invalid perturbation function" in optimizer.status
 
 def test_simulated_annealing_wrong_format_cooling_function():
-    optimizer = SimulatedAnnealing(objective_function,perturbation_function,1,stopping_criterion)
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, 1, stopping_criterion, initial_solution, cooling_factor)
     assert "[ERROR]<cooling> A controller function must be a python function object" in optimizer.status
 
 def test_simulated_annealing_missing_cooling_function():
-    optimizer = SimulatedAnnealing(objective_function,perturbation_function,{},stopping_criterion)
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, {}, stopping_criterion, initial_solution, cooling_factor)
     assert "[ERROR] Invalid cooling function" in optimizer.status
 
 def test_simulated_annealing_wrong_format_stopping_criterion_function():
-    optimizer = SimulatedAnnealing(objective_function,perturbation_function,cooling_function,1)
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, 1, initial_solution, cooling_factor)
     assert "[ERROR]<stopping_criterion> A controller function must be a python function object" in optimizer.status
 
 def test_simulated_annealing_missing_stopping_criterion_function():
-    optimizer = SimulatedAnnealing(objective_function,perturbation_function,cooling_function,{})
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, {}, initial_solution, cooling_factor)
     assert "[ERROR] Invalid stopping_criterion function" in optimizer.status
+
+def test_simulated_annealing_wrong_format_initial_solution():
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, stopping_criterion, 1, cooling_factor)
+    assert "[ERROR]<initial solution> The initial solution must be a list, pandas series or numpy ndarray" in optimizer.status
+
+def test_simulated_annealing_wrong_format_cooling_factor():
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, stopping_criterion, initial_solution, "1e-6")
+    assert "[ERROR]<cooling factor> The cooling factor must be a float or int" in optimizer.status
+
+def test_simulated_annealing_negative_cooling_factor():
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, stopping_criterion, initial_solution, -1)
+    assert "[ERROR] The cooling factor must be positive and less than 1.0" in optimizer.status
+
+def test_simulated_annealing_large_cooling_factor():
+    optimizer = SimulatedAnnealing(objective_function, perturbation_function, cooling_function, stopping_criterion, initial_solution, 1)
+    assert "[ERROR] The cooling factor must be positive and less than 1.0" in optimizer.status
