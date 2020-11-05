95f6e46bea5d5d9fdfeac07bcd0d0d2758e5330c
Renan Brito Butkeraites <rbritobut?@gmail.com>
2020-11-04 21:07:30 -0300

    Refactor function aquisition to be generic.

--- optimizer/simulated_annealing.py
+++ optimizer/simulated_annealing.py
@@ -1,16 +1,25 @@
 import numpy as np
 
 class SimulatedAnnealing:
-    def __init__(self, objective):
+    def __init__(self, objective, perturbation):
         self.status = []
-        self.__objective_aquisition(objective)
+        self.controller = {}
+        self.__function_aquisition(objective, 'objective')
+        self.__function_aquisition(perturbation, 'perturbation')
+        self.__final_validation()
 
-    def __objective_aquisition(self, objective):
-        if objective:
-            if hasattr(objective, '__call__'):
-                self.status.append("[OK] Simulated annealing created successfully")
-                self.objective = objective
+    def __function_aquisition(self, function, function_name):
+        if function:
+            if hasattr(function, '__call__'):
+                self.status.append("[OK] Aquisition of {} done successfully".format(function_name))
+                self.controller[function_name] = function
             else:
-                self.status.append("[ERROR] Objective function must be a python function object")
+                self.status.append("[ERROR]<{}> A controller function must be a python function object".format(function_name))
         else:
-            self.status.append("[ERROR] Invalid objective function")
+            self.status.append("[ERROR] Invalid {} function".format(function_name))
+
+    def __final_validation(self):
+        if any("[ERROR]" in status for status in self.status):
+            self.status.append("[ERROR] Simulated annealing creation failed")
+        else:
+            self.status.append("[OK] Simulated annealing created successfully")
--- test/test_simulated_annealing.py
+++ test/test_simulated_annealing.py
@@ -5,14 +5,27 @@ from optimizer.simulated_annealing import SimulatedAnnealing
 def objective_function(np_variable):
     return np.sum(np_variable)
 
+def perturbation_function(np_variable, n_perturbation):
+    position = np.random.randint(len(np_variable), size=n_perturbation)
+    np_variable[position] = abs(np_variable[position]-1)
+    return np_variable
+
 def test_simulated_annealing_init():
-    optimizer = SimulatedAnnealing(objective_function)
+    optimizer = SimulatedAnnealing(objective_function,perturbation_function)
     assert "[OK] Simulated annealing created successfully" in optimizer.status
 
 def test_simulated_annealing_wrong_format_objective_function():
-    optimizer = SimulatedAnnealing(1)
-    assert "[ERROR] Objective function must be a python function object" in optimizer.status
+    optimizer = SimulatedAnnealing(1,perturbation_function)
+    assert "[ERROR]<objective> A controller function must be a python function object" in optimizer.status
 
 def test_simulated_annealing_missing_objective_function():
-    optimizer = SimulatedAnnealing({})
+    optimizer = SimulatedAnnealing({},perturbation_function)
     assert "[ERROR] Invalid objective function" in optimizer.status
+
+def test_simulated_annealing_wrong_format_perturbation_function():
+    optimizer = SimulatedAnnealing(objective_function,1)
+    assert "[ERROR]<perturbation> A controller function must be a python function object" in optimizer.status
+
+def test_simulated_annealing_missing_perturbation_function():
+    optimizer = SimulatedAnnealing(objective_function,{})
+    assert "[ERROR] Invalid perturbation function" in optimizer.status
